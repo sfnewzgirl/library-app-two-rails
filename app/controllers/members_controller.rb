258ff27167a561ce472d4a3ddafcf125e6481cb7
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-04 21:07:45 -0700

    creates new member route, controller, view

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,7 @@ class MembersController < ApplicationController
   def index
     @members = Member.all
   end
+  def new
+    @member = Member.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'members#index'
+
+  get '/members', to: 'members#index', as: 'members'
+
+  get '/members/new', to: 'members#new', as: 'new_member'
+
 end
